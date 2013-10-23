d81d4539c75594be1ac47e86e576f434aeeafa8e
Artur Pogoda de la Vega <apdlv72@gmail.com>
2013-10-23 20:08:00 +0200

    Nicer Oxduino.ino: heartbeat insteat of simple blink

--- Oxduino.ino
+++ Oxduino.ino
@@ -7,10 +7,12 @@ void setup()
 	pinMode(13, OUTPUT);
 }
 
+#define ABS(A) ( ((A)<0) ? (-(A)) : (A) )
+
 void loop() 
 {
-	digitalWrite(13, HIGH);
-	delay(500);
-	digitalWrite(13, LOW);
-	delay(500);	
+	int value = millis()%3000;
+	value = value<511 ? 255-ABS(value-255) : value<1023 ? 255-ABS((value-511)-255) : 0;
+	analogWrite(13, value);
+	delay(10);
 }
